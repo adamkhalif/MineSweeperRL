92d71a46930650bca62d7b37119a8ff24f6f6e39
Mrginbringer <46897214+Mrginbringer@users.noreply.github.com>
2020-10-16 14:35:38 +0200

    Plotting

--- plot_functions.py
+++ plot_functions.py
@@ -1,10 +1,10 @@
 
 import matplotlib.pyplot as plt
 
-def plot_reward():
+def plot_reward(R, R_avg):
     rewards = plt.plot(R, alpha=.4, label='R')
     avg_rewards = plt.plot(R_avg,label='avg R')
     plt.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
     plt.xlabel('Episode')
-    plt.ylim(0, 210)
+    plt.ylim(-15, 8)
     plt.show()
