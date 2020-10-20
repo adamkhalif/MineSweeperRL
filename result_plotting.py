5f20874a1672e1edd6ec7a0c7f63f5d6a7820121
Mrginbringer <46897214+Mrginbringer@users.noreply.github.com>
2020-10-20 15:17:58 +0200

    20k

--- result_plotting.py
+++ result_plotting.py
@@ -2,7 +2,7 @@ from plot_functions import plot_reward
 from main import load_from_json
 import json
 
-filepath = "Result_RANDOM_BOMBS_35k.json"
+filepath = "Result_RANDOM_BOMBS_50k_gamma_06.json"
 data = load_from_json(filepath)
 R_avg = 0
 plot_reward(data)
