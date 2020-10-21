bea058b1fecae8b75a97b9772f3662f22155584b
Mrginbringer <46897214+Mrginbringer@users.noreply.github.com>
2020-10-21 14:47:29 +0200

    logging problem solved

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def load_from_json(filepath):
 
 if __name__ == '__main__':
     reward = [1, -1, 0.9, 0]  # win lose progress no progress
-    filepath = "Result_RANDOM_BOMBS_test.json"
-    num_episodes = 200
-    batch_size = 64
+    filepath = "Result_RANDOM_BOMBS_rerun.json"
+    num_episodes = 15000
+    batch_size = 128
     gamma = 0
     learning_rate = 1e-4
     HEIGHT = 3
     WIDTH = 3
     N_BOMBS = 1
-    eps = 0.7
+    eps = 1.
     eps_end = 0
     eps_decay = eps/(num_episodes-num_episodes*0.2)
     conv = True
